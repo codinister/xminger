6b0b9837ddbdab658a28e09ff9b7de0060d6a0da
unknown <codinister@gmail.com>
2024-04-23 15:43:19 +0100

    Commit 4

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,32 @@
+import { products } from '../../data/data';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="container">
+        <div>
+          <div className="footer-products">
+            {Object.values(products).map((v, k) => {
+              return (
+                <div
+                  key={k}
+                  style={{
+                    backgroundImage: `url(${v.img})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                  }}
+                ></div>
+              );
+            })}
+          </div>
+        </div>
 
-export default Footer
+        <div></div>
+      </div>
+
+      <div className="container">All rights reserved &copy; 2024</div>
+    </footer>
+  );
+};
+
+export default Footer;
